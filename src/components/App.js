bee686e78671b7ad922744d0951d1a01cc82bd30
Jonathan Gan <jonathang132298@gmail.com>
2019-06-24 21:00:59 -0400

    added logo

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import {
-  Button, View, TouchableOpacity,
+  Button, View, TouchableOpacity, Image, Linking
 } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import {
@@ -18,40 +18,65 @@ import {
   Header,
   Map,
 } from '.';
+// mport poly-hacks.png from '/Users/jonathan/Code/HackathonApp/src/img/poly-hacks.png';
+
+const Logo = require('../img/poly-hacks.png');
+
+const styles = {
+  logoStyle: {
+    alignSelf: 'center',
+    width: 100,
+    height: 100,
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 8,
+    marginBottom: 10,
+  }
+};
+
 
 class HomeScreen extends React.PureComponent {
-  // eslint-disable-next-line react/sort-comp
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center' }}>
+  open = () => { Linking.openURL('http://phoenixhacks.com'); };
 
-        <TouchableOpacity onPress={() => this.props.navigation.navigate('detailsPage')}>
-          <Header headerText="Details" />
+  render() {
+    const { logoStyle } = styles;
+    return (
+      <View>
+        <TouchableOpacity onPress={this.open}>
+          <Image source={Logo} style={logoStyle} />
         </TouchableOpacity>
 
-        <Button
-          title="Schedule"
-          // eslint-disable-next-line react/destructuring-assignment
-          onPress={() => this.props.navigation.navigate('schedule')}
-        />
+        <View style={{ justifyContent: 'center' }}>
+          <TouchableOpacity onPress={() => this.props.navigation.navigate('detailsPage')}>
+            <Header headerText="Details" />
+          </TouchableOpacity>
 
-        <Button
-          title="Live Updates"
+          <Button
+            title="Schedule"
           // eslint-disable-next-line react/destructuring-assignment
-          onPress={() => this.props.navigation.navigate('liveUpdate')}
-        />
+            onPress={() => this.props.navigation.navigate('schedule')}
+          />
 
-        <Button
-          title="FAQS"
+          <Button
+            title="Live Updates"
           // eslint-disable-next-line react/destructuring-assignment
-          onPress={() => this.props.navigation.navigate('faqs')}
-        />
-        <Button
-          title="Map"
-          // eslint-disable-next-line react/destructuring-assignment
-          onPress={() => this.props.navigation.navigate('map')}
-        />
+            onPress={() => this.props.navigation.navigate('liveUpdate')}
+          />
 
+          <Button
+            title="FAQS"
+          // eslint-disable-next-line react/destructuring-assignment
+            onPress={() => this.props.navigation.navigate('faqs')}
+          />
+          <Button
+            title="Map"
+          // eslint-disable-next-line react/destructuring-assignment
+            onPress={() => this.props.navigation.navigate('map')}
+          />
+
+        </View>
       </View>
     );
   }
@@ -80,6 +105,7 @@ const AppNavigator = createStackNavigator(
       headerTintColor: '#fff',
       headerTitleStyle: {
         fontWeight: 'bold',
+        textAlign: 'center',
       },
     },
   }
