0e902f24ad495d69e30949a256a9128adc9f462b
Jonathan Gan <jonathang132298@gmail.com>
2019-06-24 22:50:47 -0400

    added last pages

---
+++ src/components/Sponsors.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Sponsors extends React.PureComponent {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Sponsors</Text>
+      </View>
+    );
+  }
+}
+
+export { Sponsors };
---
+++ src/components/Workshops.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class Workshops extends React.PureComponent {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Workshops</Text>
+      </View>
+    );
+  }
+}
+
+export { Workshops };
