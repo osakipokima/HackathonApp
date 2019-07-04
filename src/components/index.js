394aa8e8a6169a79b8e45ff49574a00844418725
Jonathan Gan <jon@cryptocurrensea.io>
2019-07-04 16:38:15 -0400

    Deleting useless files

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-cycle */
 /* eslint-disable linebreak-style */
 export * from './Details';
 export * from './Schedule';
@@ -12,11 +11,8 @@ export * from './Workshops';
 export * from './Card';
 export * from './Button';
 export * from './CardSection';
-export * from './LoginForm';
 export * from './Spinner';
-export * from './Walkthrough';
 export * from './SplashScreen/index';
 export * from './Logo';
 export default './HomeScreen';
-export * from './LoginScreen';
 export * from './Textbox';
